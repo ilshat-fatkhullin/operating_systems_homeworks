b9bfc00c45b3fceeca34358f632aaef3ce2a1712
ilshat.fatkhullin <ilshat.fatkhullin@gmail.com>
2018-10-31 15:03:00 +0300

    Week 11

---
+++ week11/cmake-build-debug/CMakeFiles/week11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/week11.dir/ex4.c.o"
+  "week11.pdb"
+  "week11"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/week11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
